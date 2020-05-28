99bdd1222438fdd173d3b48a0639dfaf737f1f28
KooHaeun <heni1325@gmail.com>
2020-05-28 18:48:18 +0900

    bar

--- app/src/main/java/com/example/taxio/MainActivity.java
+++ app/src/main/java/com/example/taxio/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.taxio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar bar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar bar = findViewById(R.id.bar);
+        setSupportActionBar(bar);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+
     }
 }
