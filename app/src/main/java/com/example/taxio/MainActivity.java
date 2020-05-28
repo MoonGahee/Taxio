a2a2f39b4e4b03d23623e67f0a79ef98e2c98c43
MoonGahee <moongahee12@gmail.com>
2020-05-28 21:08:22 +0900

    상단 액션바 수정 완료 및 틀 제작 시작

--- app/src/main/java/com/example/taxio/MainActivity.java
+++ app/src/main/java/com/example/taxio/MainActivity.java
@@ -7,14 +7,16 @@ import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    Toolbar bar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bar = findViewById(R.id.abar);
-        setSupportActionBar(bar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Toolbar bar = (Toolbar)findViewById(R.id.abar); // 툴바를 액티비티의 앱바로 지정
+        setSupportActionBar(bar); //툴바를 현재 액션바로 설정
+        ActionBar actionBar = getSupportActionBar(); //현재 액션바를 가져옴
+        actionBar.setDisplayShowTitleEnabled(false); //액션바의 타이틀 삭제
+        actionBar.setDisplayHomeAsUpEnabled(true); //홈으로 가기 버튼 활성화
 
     }
 }
