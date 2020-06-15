f09db7f2e9ecc6188fa2bd8071a0c18cdb3e5440
JuhyePark <ju970224@naver.com>
2020-06-15 10:52:39 +0900

    지역선택에서 화면 넘어가게!

--- app/src/main/java/com/example/taxio/NewScheduleRegion.java
+++ app/src/main/java/com/example/taxio/NewScheduleRegion.java
@@ -1,18 +1,56 @@
 package com.example.taxio;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class NewScheduleRegion extends AppCompatActivity {
+
+    Toolbar toolbar;
+    ImageView map;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select_region);
 
+        toolbar = (Toolbar)findViewById(R.id.abar); // 툴바를 액티비티의 앱바로 지정
+        setSupportActionBar(toolbar); //툴바를 현재 액션바로 설정
+        ActionBar actionBar = getSupportActionBar(); //현재 액션바를 가져옴
+        actionBar.setDisplayShowTitleEnabled(false); //액션바의 타이틀 삭제
+        actionBar.setDisplayHomeAsUpEnabled(true); //홈으로 가기 버튼 활성화
+
+        map = findViewById(R.id.imageKorea);
+
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(NewScheduleRegion.this);
+                builder.setTitle("지역 확인");
+                builder.setMessage("선택하신 지역이 제주도가 맞습니까?");
+                builder.setPositiveButton("네", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(NewScheduleRegion.this, TripSchedule.class);
+                        startActivity(intent);
+                    }
+                });
+                builder.setNegativeButton("아니요", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+            }
+        });
     }
 }
