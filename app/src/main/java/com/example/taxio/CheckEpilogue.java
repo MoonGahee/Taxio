ecb14762794b4951e513340f563b87be7595a615
Kwanwoo-park <65990897+Kwanwoo-park@users.noreply.github.com>
2020-06-15 15:51:02 +0900

    홈버튼과 뒤로 가기 버튼

--- app/src/main/java/com/example/taxio/CheckEpilogue.java
+++ app/src/main/java/com/example/taxio/CheckEpilogue.java
@@ -6,6 +6,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
@@ -15,6 +16,7 @@ import androidx.appcompat.widget.Toolbar;
 public class CheckEpilogue extends AppCompatActivity {
 
     Toolbar toolbar;
+    TextView title_text;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,6 +28,18 @@ public class CheckEpilogue extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar(); //현재 액션바를 가져옴
         actionBar.setDisplayShowTitleEnabled(false); //액션바의 타이틀 삭제
         actionBar.setDisplayHomeAsUpEnabled(true); //홈으로 가기 버튼 활성화
+
+        title_text = findViewById(R.id.title_text);
+        title_text.setClickable(true);
+
+        title_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(CheckEpilogue.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 
     public void goMain(ImageView logo){//로고버튼 클릭시
--- app/src/main/java/com/example/taxio/TripSchedule.java
+++ app/src/main/java/com/example/taxio/TripSchedule.java
@@ -5,14 +5,39 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class TripSchedule extends AppCompatActivity {
+
+    TextView title_text;
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        toolbar = (Toolbar)findViewById(R.id.abar); // 툴바를 액티비티의 앱바로 지정
+        setSupportActionBar(toolbar); //툴바를 현재 액션바로 설정
+        ActionBar actionBar = getSupportActionBar(); //현재 액션바를 가져옴
+        actionBar.setDisplayShowTitleEnabled(false); //액션바의 타이틀 삭제
+        actionBar.setDisplayHomeAsUpEnabled(true); //홈으로 가기 버튼 활성화
+
+        title_text = findViewById(R.id.title_text);
+        title_text.setClickable(true);
+
+        title_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(TripSchedule.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 
     public void goMain(ImageView logo){//로고버튼 클릭시
